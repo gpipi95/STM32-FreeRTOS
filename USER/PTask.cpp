85d3fef6037f98b0a347ce76892c2f729e12b30a
pipi95 <pipi95@163.com>
2024-03-02 13:40:19 +0800

    修正.clang-format配置文件的名称，正在修改PTask类。

--- USER/PTask.cpp
+++ USER/PTask.cpp
@@ -1,9 +1,36 @@
 #include "PTask.h"
 
-PTask::PTask()
+using namespace std;
+
+PTask::PTask(const string& name, const uint16_t stackSize, uint16_t priority)
+    : _task(NULL)
+    , _taskName(name)
+    , _stackSize(stackSize)
+    , _priority(priority)
 {
+    BaseType_t xReturned = xTaskCreate((TaskFunction_t)_cyclicJob,
+                                       (const char*)_taskName.c_str(),
+                                       _stackSize,
+                                       (void*)this,
+                                       (UBaseType_t)_priority,
+                                       _task);
+    if (xReturned != pdPASS) {
+        /* The task was created. */
+        _task = NULL;
+    }
 }
 
 PTask::~PTask()
 {
+    if (NULL != _task) {
+        vTaskDelete(_task);
+    }
+}
+
+void PTask::_cyclicJob(PTask* task)
+{
+    while (true) {
+        task->work();
+        vTaskDelay(10);
+    }
 }
--- USER/PTask.h
+++ USER/PTask.h
@@ -1,15 +1,27 @@
-#ifndef TASK_H
-#define TASK_H
-
 #pragma once
+#ifndef PTASK_H
+#define PTASK_H
 
-class PTask
-{
+#include <string>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+class PTask {
 public:
-    PTask();
-    ~PTask();
+    PTask(const std::string& name, uint16_t stackSize, uint16_t priority);
+    virtual ~PTask();
+
+    bool Start();
+
+protected:
+    virtual bool work();
 
 private:
+    static void    _cyclicJob(PTask* task);
+    TaskHandle_t*  _task;
+    std::string    _taskName;
+    const uint16_t _stackSize;
+    uint16_t       _priority;
 };
-
 #endif
