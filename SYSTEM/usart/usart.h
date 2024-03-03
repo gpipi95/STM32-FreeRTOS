de6c8d6477ad6d3e5017738fb3f7b99b63d90a9f
pipi95 <pipi95@163.com>
2024-03-03 21:56:50 +0800

    正在初始化UART3过程中，还需要对其进行封装。

--- SYSTEM/usart/usart.h
+++ SYSTEM/usart/usart.h
@@ -8,14 +8,14 @@
 #define EN_USART1_RX 1    // 使能（1）/禁止（0）串口1接收
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-    extern u8 USART_RX_BUF[USART_REC_LEN]; // 接收缓冲,最大USART_REC_LEN个字节.末字节为换行符
-    extern u16 USART_RX_STA;               // 接收状态标记
-    // 如果想串口中断接收，请不要注释以下宏定义
-    void uart_init(u32 bound);
+extern u8  USART_RX_BUF[USART_REC_LEN]; // 接收缓冲,最大USART_REC_LEN个字节.末字节为换行符
+extern u16 USART_RX_STA;                // 接收状态标记
+// 如果想串口中断接收，请不要注释以下宏定义
+void uart_init(u32 bound);
+void uart1_init(u32 bound);
 
 #ifdef __cplusplus
 }
--- USER/pSys.c
+++ USER/pSys.c
@@ -9,7 +9,7 @@ bool initSystem(void)
 {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4); // 设置系统中断优先级分组4
     delay_init(168);                                // 初始化延时函数
-    uart_init(115200);                              // 初始化串口
+    uart1_init(115200);                             // 初始化串口
     LED_Init();                                     // 初始化LED端口
     return true;
 }
